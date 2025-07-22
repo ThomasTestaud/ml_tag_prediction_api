f725a6aae8820926760c4916effafcbb8fddf8b9
*Stratt321963 <thomas.testaud@hotmail.fr>
2025-07-22 09:09:15 +0200

    Fixed pytest

--- api.py
+++ api.py
@@ -33,5 +33,6 @@ def predict_tags_route():
 
 import os
 
-PORT = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=PORT)
+if __name__ == "__main__":
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
